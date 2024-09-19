a680e98aeec3f3ca474c143aacab23b5f7324dee
Amine CHOGRANI <aminechograni@gmail.com>
2024-09-19 15:17:42 +0100

    create Db Context

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -4,5 +4,9 @@ namespace dotnetEightAPI.Data
 {
     public class AppDbContext:DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
     }
 }
